fbc304b6c19c8f511d4edfb599d4363ebdb767da
mingoo <83053171+panggu15@users.noreply.github.com>
2024-11-17 20:35:32 +0900

    Update test.py

--- test.py
+++ test.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
         prompts = [target_prompt] + ([""] * (len(ref_prompts))) 
     else:
         prompts = [target_prompt] + ref_prompts
-    negative_prompts = [cfg.negative_prompt] * 2
+    negative_prompts = [cfg.negative_prompt] * len(prompts)
 
     # set dirs
     concepts_name  = ref_image_path.split('/')[3]
