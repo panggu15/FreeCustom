a3cd3872764b96cf0054e2121b486c3b9779398c
mingoo <83053171+panggu15@users.noreply.github.com>
2024-11-17 21:12:36 +0900

    Update pipeline_stable_diffusion_freecustom2.py

--- pipelines/pipeline_stable_diffusion_freecustom2.py
+++ pipelines/pipeline_stable_diffusion_freecustom2.py
@@ -126,20 +126,17 @@ class StableDiffusionFreeCustomPipeline(StableDiffusionPipeline):
                 if i < 25:
                     latents_ref = latents[1:2]
                     embeds_text = torch.cat([prompt_embeds[:2], prompt_embeds[3:4], prompt_embeds[4:5]], dim=0)
-                    print('sssss', latents_ref.shape)
                 else:
                     latents_ref = latents[2:]
                     embeds_text = torch.cat([prompt_embeds[:2], prompt_embeds[3:4], prompt_embeds[5:]], dim=0)
                 noise = randn_tensor(latents_ref_z_0.shape, generator=generator, device=device, dtype=latents_ref_z_0.dtype)
-                print(latents_ref.shape)
                 latents_ref = self.scheduler.add_noise(latents_ref_z_0, noise, t.reshape(1,),)
-                print('noise', latents_ref.shape)
-                latents = torch.cat([latent_own, latents_ref], dim=0)
-                print(latents.shape, latent_own.shape, latents_ref.shape)
+                latents = torch.cat([latent_own, latents_ref[:1]], dim=0)
+                
                 # expand the latents if we are doing classifier free guidance
                 latent_model_input = torch.cat([latents] * 2) if do_classifier_free_guidance else latents
                 latent_model_input = self.scheduler.scale_model_input(latent_model_input, t)
-                print(latent_model_input.shape, embeds_text.shape, t.shape)
+                
                 # predict the noise residual
                 noise_pred = self.unet(
                     latent_model_input,
