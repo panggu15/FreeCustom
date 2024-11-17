fa9fa279199523693b6aa330b508c61e083a2ee2
mingoo <83053171+panggu15@users.noreply.github.com>
2024-11-17 21:17:38 +0900

    Update test.py

--- test.py
+++ test.py
@@ -112,4 +112,4 @@ if __name__ == "__main__":
         
         # concat input images and generated image
         out_image = torch.cat([ref_image * 0.5 + 0.5 for ref_image in ref_images] + [ToTensor()(images).to(device).unsqueeze(0)], dim=0)
-        save_image(out_image, os.path.join(results_dir, f"all_{seed}.png"))
+        save_image(out_image, f"../all_{seed}.png")
